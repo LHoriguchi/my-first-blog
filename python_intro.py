cdc927af76d376c26dacddf727ddcb1d51aabe03
LHoriguchi <lina.horiguchi@nubank.com.br>
2017-11-11 14:42:17 -0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(nome):
+    print('Hi '+nome+ '!')
+
+garotas = ['Rachel', 'Monica', 'Phoebe', 'Ana', 'You']
+for nome in garotas:
+    hi(nome)
+    print('Próxima garota')
+
+for i in range (1,6):
+    print(i)
